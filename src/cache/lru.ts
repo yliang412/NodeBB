7d0150ae72f98d951cecf38517ade61de0758e96
Yuchen Liang <yuchenl3@andrew.cmu.edu>
2023-09-04 20:39:20 -0400

    feat: translate src/cache/lru.js from JS to TS
    Signed-off-by: Yuchen Liang <yuchenl3@andrew.cmu.edu>

--- src/cache/lru.ts
+++ src/cache/lru.ts
@@ -1,16 +1,43 @@
 'use strict';
-module.exports = function (opts) {
+
+interface Options {
+    name: string,
+    length: number,
+    enabled: boolean,
+}
+
+interface Cache {
+    name: string,
+    hits: number,
+    misses: number,
+    enabled: boolean,
+
+    set(key: any, value: any, ttl: number): any,
+    get(key: any): any,
+    del(keys: any|any[]): any,
+    delete(keys: any|any[]): any,
+    reset(): void,
+    clear(): void,
+    getUnCachedKeys(keys: any[], cachedData: any) : any,
+    dump(): any[],
+    peek(key: any, opts?: { allowStale?: boolean }): any,
+}
+
+module.exports = function (opts: Options) {
     const LRU = require('lru-cache');
     const pubsub = require('../pubsub');
+
     // lru-cache@7 deprecations
     const winston = require('winston');
     const chalk = require('chalk');
+
     // sometimes we kept passing in `length` with no corresponding `maxSize`.
     // This is now enforced in v7; drop superfluous property
     if (opts.hasOwnProperty('length') && !opts.hasOwnProperty('maxSize')) {
         winston.warn(`[cache/init(${opts.name})] ${chalk.white.bgRed.bold('DEPRECATION')} ${chalk.yellow('length')} was passed in without a corresponding ${chalk.yellow('maxSize')}. Both are now required as of lru-cache@7.0.0.`);
         delete opts.length;
     }
+
     const deprecations = new Map([
         ['stale', 'allowStale'],
         ['maxAge', 'ttl'],
@@ -23,13 +50,16 @@ module.exports = function (opts) {
             delete opts[oldProp];
         }
     });
+
     const lruCache = new LRU(opts);
-    const cache = {};
+
+    const cache = {} as Cache;
     cache.name = opts.name;
     cache.hits = 0;
     cache.misses = 0;
     cache.enabled = opts.hasOwnProperty('enabled') ? opts.enabled : true;
     const cacheSet = lruCache.set;
+
     // expose properties while keeping backwards compatibility
     const propertyMap = new Map([
         ['length', 'calculatedSize'],
@@ -49,16 +79,22 @@ module.exports = function (opts) {
             enumerable: true,
         });
     });
+
+    interface LRUOptions {
+        ttl: number,
+    }
+
     cache.set = function (key, value, ttl) {
         if (!cache.enabled) {
             return;
         }
-        const opts = {};
+        const opts = {} as LRUOptions;
         if (ttl) {
             opts.ttl = ttl;
         }
         cacheSet.apply(lruCache, [key, value, opts]);
     };
+
     cache.get = function (key) {
         if (!cache.enabled) {
             return undefined;
@@ -66,12 +102,12 @@ module.exports = function (opts) {
         const data = lruCache.get(key);
         if (data === undefined) {
             cache.misses += 1;
-        }
-        else {
+        } else {
             cache.hits += 1;
         }
         return data;
     };
+
     cache.del = function (keys) {
         if (!Array.isArray(keys)) {
             keys = [keys];
@@ -80,24 +116,29 @@ module.exports = function (opts) {
         keys.forEach(key => lruCache.delete(key));
     };
     cache.delete = cache.del;
+
     cache.reset = function () {
         pubsub.publish(`${cache.name}:lruCache:reset`);
         localReset();
     };
     cache.clear = cache.reset;
+
     function localReset() {
         lruCache.clear();
         cache.hits = 0;
         cache.misses = 0;
     }
+
     pubsub.on(`${cache.name}:lruCache:reset`, () => {
         localReset();
     });
+
     pubsub.on(`${cache.name}:lruCache:del`, (keys) => {
         if (Array.isArray(keys)) {
             keys.forEach(key => lruCache.delete(key));
         }
     });
+
     cache.getUnCachedKeys = function (keys, cachedData) {
         if (!cache.enabled) {
             return keys;
@@ -112,17 +153,21 @@ module.exports = function (opts) {
             }
             return !isCached;
         });
+
         const hits = keys.length - unCachedKeys.length;
         const misses = keys.length - hits;
         cache.hits += hits;
         cache.misses += misses;
         return unCachedKeys;
     };
+
     cache.dump = function () {
         return lruCache.dump();
     };
+
     cache.peek = function (key) {
         return lruCache.peek(key);
     };
+
     return cache;
 };
